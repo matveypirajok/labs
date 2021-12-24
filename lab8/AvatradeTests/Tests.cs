7dd474f2b223549135d9cb5a3ccb53e251bcb9b5
matvey <goloviy.2013@gmail.com>
2021-12-24 17:04:20 +0300

    fixed lab8

--- lab8/AvatradeTests/Tests.cs
+++ lab8/AvatradeTests/Tests.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
+using System.Threading;
 
 namespace AvatradeTests
 {
@@ -23,34 +24,68 @@ namespace AvatradeTests
             _driver.Navigate().GoToUrl("https://capital.com/");
             _driver.Manage().Window.Maximize();
 
-            var logInBtn = _driver.FindElement(By.XPath("//div[@class='banner__slider-btn--with-link']//div[@class='bannerBtns']//a[@class='button-main outlined-light rounded-lg ln-auto _demo showBannerBtns __cp_b __cp_bs']"));
+            var logInBtn = _driver.FindElement(By.XPath("//a[@class = 'button-main ln-auto stroke-white']"));
             logInBtn.Click();
 
             _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
-            var inputEmail = _wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@class='control-outline control-outline--input']//div[@class='control-form']//input[@class='field-form']")));
+            var inputEmail = _wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@name = 'email']")));
             inputEmail.SendKeys(TestEmail);
 
-            var inputPassword = _driver.FindElement(By.XPath("//div[@class='control-outline control-outline--input']//div[@class='control-form']//input[@class='field-form']"));
+            var inputPassword = _driver.FindElement(By.XPath("//input[@name = 'password']"));
             inputPassword.SendKeys(TestPassword);
 
-            var submitBtn = _driver.FindElement(By.XPath("//button[@class='button-main s2_btn']"));
+            var submitBtn = _driver.FindElement(By.XPath("//button[@class = 'button-main l_btn']"));
             submitBtn.Click();
         }
 
+        
         [Test]
         public void CreateDealToBuyEURTest()
         {
-            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+            string Element = "EUR/USD";
 
-            var selectCurrency = _wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[text()='EUR/JPY']"))); //по непонятной причине работает через раз
+            Thread.Sleep(10000);
+
+            var AccountClick = _driver.FindElement(By.XPath("/html/body/app-root/div/topbar/div/account-info"));
+            AccountClick.Click();
+
+            var DemoClick = _driver.FindElement(By.XPath("/html/body/app-root/div/topbar/div/account-info/div/div[3]/div[4]"));
+            DemoClick.Click();
+
+            Thread.Sleep(3000);
+
+            var selectCurrency = _driver.FindElement(By.XPath("//div[text()='EUR/USD']"));
             selectCurrency.Click();
 
-            var buyprikoli = _driver.FindElement(By
-                .XPath("//button[@class='button-outlined button-outlined--small']"));
-            buyprikoli.Click();
+            var buyEUR = _driver.FindElement(By.XPath("//button[@class='button-outlined button-outlined--small']"));
+            buyEUR.Click();
+            
+            Thread.Sleep(1000);
 
-            var selectCurrencyy = _wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//button[@class='button-main button-main--negative ng-star-inserted']")));
+            var ImputCount = _driver.FindElement(By.XPath("/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/trade-side-panel/div/div/div[3]/trade-deal-ticket/place-order/div/scroll-pane/div[1]/div[1]/input-number2/div/input"));
+            ImputCount.Click();
+
+            Thread.Sleep(1000);
+
+            var inputCountEUR = _driver.FindElement(By.XPath("/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/trade-side-panel/div/div/div[3]/trade-deal-ticket/place-order/div/scroll-pane/div[1]/div[1]/input-number2/div/input"));
+            inputCountEUR.Clear();
+
+            Thread.Sleep(3000);
+
+            var selectCurrencyy = _driver.FindElement(By.XPath("/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/trade-side-panel/div/div/div[3]/trade-deal-ticket/place-order/div/div/button"));
             selectCurrencyy.Click();
+
+            Thread.Sleep(3000);
+
+            var buttonPortf = _driver.FindElement(By.XPath("/html/body/app-root/div/left-side-panel/div[2]/floating-left-panel/div[1]/div[4]"));
+            buttonPortf.Click();
+
+            Thread.Sleep(3000);
+
+            var searchEUR = _driver.FindElement(By.XPath("//div[text()=' EUR/USD ']")).Text;
+            Assert.AreEqual(Element, searchEUR);
+
+
 
         }
 
--- lab9/AvatradeTests/PageObject/TradingPageObject.cs
+++ lab9/AvatradeTests/PageObject/TradingPageObject.cs
@@ -12,7 +12,7 @@ namespace AvatradeTests.PageObject
         private IWebDriver _webDriver;
         private By SearchItem = By.XPath("//div[text()='EUR/JPY']");
         private By AddItem = By.XPath("//button[@class='button-outlined button-outlined--small']");
-        private By Confirm = By.XPath("//button[@class='button-main button-main--negative']");// ГРАДОВЕЦ КЛОУН 
+        private By Confirm = By.XPath("/html/body/app-root/div/left-side-panel/div[1]/div/div[2]/trade-view/trade-side-panel/div/div/div[3]/trade-deal-ticket/place-order/div/div/button");
         public TradingPageObject(IWebDriver webDriver)
         {
             _webDriver = webDriver;
